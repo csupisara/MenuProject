b7d83473574f3233490c1f451888b927d9d8ff72
Supisara Chuthathumpitak <csupisara@gmail.com>
2017-05-20 03:56:13 +0700

    updated

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -2,17 +2,52 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class MainController {
 
-	
 	@FXML //Acts as a wire that connect the FXML file with MainController.
-//	private Label myMessage;
-	private Menu menu;
-	public void generateRandom(ActionEvent event){
+	//	private Label myMessage;
+	private Tab menu;
+
+	@FXML
+	private Tab status;
+
+	@FXML
+	private TableColumn<String, String> check;
+
+	@FXML
+	private TableColumn<String, String> order;
+
+	@FXML
+	private TableColumn<String, String> picture;
+
+	@FXML
+	private TableColumn<String, String> price;
+
+	@FXML
+	private ProgressIndicator progress;
+
+	@FXML
+	private Button confirm;
+	public void confirm(ActionEvent event){
+
+	}
+
+	@FXML
+	private Button clear;
+	public void clear(ActionEvent event){
+		
 	}
 	
+	@FXML
+	private TableView<TableColumn> table;
 	
+	@FXML
+	private SplitPane myMenu;
 }
