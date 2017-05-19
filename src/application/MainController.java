20f7deed249dd288db5d829d49ba3507cdfc9111
Supisara Chuthathumpitak <csupisara@gmail.com>
2017-05-20 00:21:02 +0700

    first

---
+++ src/application/MainController.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+
+public class MainController {
+
+	
+	@FXML //Acts as a wire that connect the FXML file with MainController.
+//	private Label myMessage;
+	private Menu menu;
+	public void generateRandom(ActionEvent event){
+	}
+	
+	
+}
