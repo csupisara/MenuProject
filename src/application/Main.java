33c4ed24d2574790253be54a70a25ce25a6e5c75
Sathira Kittisukmongkol <sathira.k@ku.th>
2017-05-20 16:57:21 +0700

    Commit ffrom jamieBranch

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,19 +5,36 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 
 
 public class Main extends Application {
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 //			BorderPane root = new BorderPane();
-			Parent root = FXMLLoader.load(ClassLoader.getSystemResource("application/MyMain.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+//			Parent root = FXMLLoader.load(ClassLoader.getSystemResource("application/MyMain.fxml"));
+//			Scene scene = new Scene(root,400,400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
+			
+			primaryStage.setTitle( "JamieFX GUI" );
+			
+			Button button1 = new Button();
+			button1.setText( "Clicked Me!" );
+			
+			StackPane layout = new StackPane();
+			layout.getChildren().add( button1 );
+			
+			Scene scene = new Scene( layout , 300 , 250 );
+			
+			primaryStage.setScene( scene );
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
