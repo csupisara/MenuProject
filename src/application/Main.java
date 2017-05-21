e2e0db9a1020eac808e86040fe7b6395a0f81fd4
Sathira Kittisukmongkol <sathira.k@ku.th>
2017-05-21 13:40:09 +0700

    Can run

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,12 @@
 package application;
-	
+import javafx.*;
+import javafx.fxml.Initializable;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
@@ -12,10 +14,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-//			BorderPane root = new BorderPane();
 			Parent root = FXMLLoader.load(ClassLoader.getSystemResource("application/MyMain.fxml"));
+			
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add( getClass().getResource("application.css").toExternalForm() );
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
