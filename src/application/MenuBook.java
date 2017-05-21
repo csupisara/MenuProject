896200273c2b6b02e7f07888bd9e3dcb5a77d99a
Supisara Chuthathumpitak <csupisara@gmail.com>
2017-05-21 21:07:18 +0700

    adding memberID label

--- src/application/MenuBook.java
+++ src/application/MenuBook.java
@@ -2,12 +2,10 @@ package application;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MenuBook {
