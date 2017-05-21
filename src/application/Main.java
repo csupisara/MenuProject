25a55e0f75fe96a52a19a98fb6a1bc0560f0c344
Supisara Chuthathumpitak <csupisara@gmail.com>
2017-05-21 15:58:27 +0700

    adding table

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,30 +1,16 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-
-		//BorderPane root = new BorderPane();
-
-		//  Image image = new Image(getClass().getResourceAsStream("sushi.jpg"));
-		//
-		//  Button button = new Button("Naoki", new ImageView(image));
-		//
-		//  button.setOnAction(e -> System.out.println("naoki"));
-		//  HBox layout = new HBox();
-		//  layout.getChildren().addAll(button);
+	public void start(Stage primaryStage) throws IOException {
 
 		try {
 			Parent root = FXMLLoader.load(ClassLoader.getSystemResource("application/MyMain.fxml"));
@@ -35,6 +21,8 @@ public class Main extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		
+		MainController mainController = new MainController();
 
 	}
 
--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,9 +1,16 @@
 package application;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import com.jfoenix.controls.JFXButton;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
@@ -14,77 +21,71 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 
-public class MainController {
+public class MainController implements Initializable {
+	
+	public ObservableList<Menu> list;
+	@FXML private TableView<Menu> confirmTableView;
+	@FXML private TableColumn<Menu, Integer> menuTableColumn1;
+	@FXML private TableColumn<Menu, String> menuTableColumn2;
+	@FXML private TableColumn<Menu, Integer> menuTableColumn3;
+	@FXML private TableColumn<Menu, Integer> menuTableColumn4;
+	
+	public MainController() throws IOException {
+		menu();
+	}
 
-//	@FXML
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		menuTableColumn1.setCellValueFactory(new PropertyValueFactory<Menu, Integer>("menuTableColumn1"));
+		menuTableColumn2.setCellValueFactory(new PropertyValueFactory<Menu, String>("menuTableColumn2"));
+		menuTableColumn3.setCellValueFactory(new PropertyValueFactory<Menu, Integer>("menuTableColumn3"));
+		menuTableColumn4.setCellValueFactory(new PropertyValueFactory<Menu, Integer>("menuTableColumn4"));
+		
+		confirmTableView.setItems(list);
+	}
+
+	public void menu() throws IOException{
+		list = FXCollections.observableArrayList(new Menu(0,"MMMM",200));
+	}
 	
-	@FXML
-	private JFXButton test;
-	
-	@FXML
-	private HBox hBox;
-	
-	@FXML
-	private TabPane tabPane;
-	
+	@FXML private JFXButton test;
+
+	@FXML private HBox hBox;
+
+	@FXML private TabPane tabPane;
+
 	@FXML //Acts as a wire that connect the FXML file with MainController.
 	private Tab menuTab;
-	
-	@FXML
-	private ButtonBar naokiButton;
-	
-	@FXML
-	private ImageView naoki;
-	
-	@FXML
-	private Label menuName;
-	
-	@FXML
-	private Label price;
-	
-	@FXML
-	private AnchorPane confirmPane;
-	
-	@FXML
-	private AnchorPane confirmTablePane;
-	
-	@FXML
-	private TableView<TableColumn<String, String>> confirmTableView;
-	
-	@FXML
-	private TableColumn<String, String> menuTableColumn1;
-	
-	@FXML
-	private TableColumn<String, String> menuTableColumn2;
-	
-	@FXML
-	private TableColumn<String, String> menuTableColumn3;
-	
-	@FXML
-	private TableColumn<String, String> menuTableColumn4;
-	
-	@FXML
-	private TableColumn<String, String> statusTableColumn1;
-	
-	@FXML
-	private TableColumn<String, String> statusTableColumn2;
-	
-	@FXML
-	private TableColumn<String, String> statusTableColumn3;
-	
-	@FXML
-	private TableColumn<String, String> statusTableColumn4;
-	
-	@FXML
-	private TableColumn<String, String> statusTableColumn5;
-	
-	@FXML
-	private AnchorPane totalPane;
-	
+
+	@FXML private ButtonBar naokiButton;
+
+	@FXML private ImageView naoki;
+
+	@FXML private Label menuName;
+
+	@FXML private Label price;
+
+	@FXML private AnchorPane confirmPane;
+
+	@FXML private AnchorPane confirmTablePane;
+
+	@FXML private TableColumn<String, String> statusTableColumn1;
+
+	@FXML private TableColumn<String, String> statusTableColumn2;
+
+	@FXML private TableColumn<String, String> statusTableColumn3;
+
+	@FXML private TableColumn<String, String> statusTableColumn4;
+
+	@FXML private TableColumn<String, String> statusTableColumn5;
+
+	@FXML private AnchorPane totalPane;
+
 	@FXML
 	private Button confirm;
 	public void confirm(ActionEvent event){
@@ -102,29 +103,28 @@ public class MainController {
 		menuTableColumn3.setText("");
 		menuTableColumn4.setText("");
 	}
-	
+
 	@FXML
 	private Button checkbill;
 	public void checkbill(ActionEvent event){
+
 	}
-	
+
 	@FXML private ScrollBar scrollbar;
-	
+
 	@FXML
 	private Label totalLabel;
 	public void totalLabel(){
 		menuTableColumn3.getText();
 	}
-	
-	@FXML
-	private Label itemLabel;
 
-	@FXML
-	private Tab statusTab;
-	
-	@FXML
-	private AnchorPane statusPane;
+	@FXML private Label itemLabel;
 
-	@FXML
-	private ProgressIndicator progress;
+	@FXML private Tab statusTab;
+
+	@FXML private AnchorPane statusPane;
+
+	@FXML private ProgressIndicator progress;
+
+
 }
